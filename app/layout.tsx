d0c16c8cad5b1d0acbd9f65fef53310b83d636cb
Azri Ibrahim <azriibrahim@Azris-MacBook-Air.local>
2025-11-30 21:41:56 +0800

    Add favicon icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,11 @@ export const metadata: Metadata = {
   verification: {
     google: 'your-google-verification-code',
   },
+  icons: {
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
